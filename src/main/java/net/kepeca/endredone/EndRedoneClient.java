ccb1cf89f24cd8fbb417dba2b6ca79d7bb1140e1
erikkepica <erik.znider@gmail.com>
2024-04-27 11:29:55 +0200

    Initial commit

---
+++ src/main/java/net/kepeca/endredone/EndRedoneClient.java
@@ -0,0 +1,10 @@
+package net.kepeca.endredone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndRedoneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kepeca/endredone/EndRedoneDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kepeca.endredone;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EndRedoneDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
